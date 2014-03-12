b29deee919e3bc75cbe910be8fcded418c38c406
Alex Yang <alyx.yang@gmail.com>
2014-03-11 18:30:46 -0700

    added rails project

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,18 @@
+class HomeController < ApplicationController
+  require 'json'
+  def index
+  end
+
+  def recipe
+  end
+
+  def category
+  end
+
+  def blog_post
+  end
+
+  def blog
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstaApp::Application.config.secret_token = 'df28551f94c753e555b0acfb86a3bc963a012cb654383e521feb553b304495d91923a22786c92dd5e5735147ccd64f3ecf04ee674fe5a6df5b743094747e7b33'
